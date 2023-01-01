d8b94895121eae4d8600afff7fa9029bbd01bdb4
Rasika <121392298+rasika-khapate@users.noreply.github.com>
2023-01-01 21:03:17 +0530

    working code

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ var textOutput = document.querySelector("#output");
 
 btnTranslate.addEventListener("click", clickEventHandler)
 
+// var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text)
+{
+  return serverURL + "?" +"text=" + text
+}
+
+function errorHandler(error)
+{
+  console.log("error occured", error);
+  alert("something seems wrong with the server, please try again after some time");
+}
+
 function clickEventHandler() 
 {
-  textOutput.innerText = "translated to " + textInput.value
+  // textOutput.innerText = "translated to " + textInput.value
+
+var inputText = textInput.value;
+
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json =>
+    {var translatedText = json.contents.translated
+      textOutput.innerText = translatedText}
+    )
+  .catch(errorHandler)
 }
